46a21aa0a63d855d9e30cdf5c894e4631f86e6ac
Matt Hotstone <matt_hotstone@hotmail.com>
2014-01-02 12:59:56 +0000

    Converted test project from MSTest to NUnit.

--- Femah.Core.Tests/Properties/AssemblyInfo.cs
+++ Femah.Core.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Femah.Core.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f3d8f6bf-c2b8-40ff-be73-003810de42a1")]
+[assembly: Guid("b3f9334e-17c7-4e0d-8df1-a0382bc154eb")]
 
 // Version information for an assembly consists of the following four values:
 //
