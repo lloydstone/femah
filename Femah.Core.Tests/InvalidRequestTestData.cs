fd8f195bc45a8a9ac46a7d14eea959daa4a4cecd
AwaNoodle <mark.nick.turner@gmail.com>
2014-03-21 08:06:55 +0000

    Added names to invalid request test cases

--- Femah.Core.Tests/InvalidRequestTestData.cs
+++ Femah.Core.Tests/InvalidRequestTestData.cs
@@ -11,16 +11,16 @@ namespace Femah.Core.Tests
             {
                 yield return new TestCaseData("TestFeatureSwitch", "This is not JSON",
                     "\"Error: Unable to deserialise the request body.  Either the JSON is invalid or the supplied 'FeatureType' value is incorrect, have you used the AssemblyQualifiedName as the 'FeatureType' in the request?\"",
-                    HttpStatusCode.BadRequest);
+                    HttpStatusCode.BadRequest).SetName("GivenRequestWithInvalidJson_ThenHttpCode400AndErrorAreReturned");
                 yield return new TestCaseData("TestFeatureSwitch",
                     "{{\"IsEnabled\":NotValidBoolean,\"Name\":\"TestFeatureSwitch1\",\"FeatureType\":\"Femah.Core.FeatureSwitchTypes.SimpleFeatureSwitch, Femah.Core, Version=0.1.0.0, Culture=neutral, PublicKeyToken=null\",\"Description\":\"Define a short description of the feature switch type here.\",\"ConfigurationInstructions\":\"Add configuration context and instructions to be displayed in the admin UI\"}}",
                     "\"Error: Unable to deserialise the request body.  Either the JSON is invalid or the supplied 'FeatureType' value is incorrect, have you used the AssemblyQualifiedName as the 'FeatureType' in the request?\"",
-                    HttpStatusCode.BadRequest);
+                    HttpStatusCode.BadRequest).SetName("GivenRequestWithInvalidEnabledValue_ThenHttpCode400AndErrorAreReturned");
                 yield return
                     new TestCaseData("TestFeatureSwitch",
                         "{{\"IsEnabled\":true,\"Name\":\"TestFeatureSwitch1\",\"FeatureType\":\"Invalid.FeatureType.Will.Not.Deserialise\",\"Description\":\"Define a short description of the feature switch type here.\",\"ConfigurationInstructions\":\"Add configuration context and instructions to be displayed in the admin UI\"}}",
                         "\"Error: Unable to deserialise the request body.  Either the JSON is invalid or the supplied 'FeatureType' value is incorrect, have you used the AssemblyQualifiedName as the 'FeatureType' in the request?\"",
-                        HttpStatusCode.BadRequest);
+                        HttpStatusCode.BadRequest).SetName("GivenRequestWithInvalidFeatureType_ThenHttpCode400AndErrorAreReturned"); ;
             } 
         }
     }
