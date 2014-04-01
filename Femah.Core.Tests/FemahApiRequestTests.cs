92bf3c510b1a98d81567cc0fd45a51daac36628f
AwaNoodle <mark.nick.turner@gmail.com>
2014-04-01 23:16:12 +0100

    Moved service not found and parameter querying not supported tests to FemahApiRequestTests

--- Femah.Core.Tests/FemahApiRequestTests.cs
+++ Femah.Core.Tests/FemahApiRequestTests.cs
@@ -7,10 +7,32 @@ using Shouldly;
 
 namespace Femah.Core.Tests
 {
+    using FeatureSwitchTypes;
+
     public class FemahApiRequestTests
     {
         public class TheProcessRequestMethod
         {
+            [Test]
+            public void Returns405IfServiceIsNotFound()
+            {
+                //Arrange
+                var testable = new TestableFemahApiHttpHandler();
+                var httpContextMock = new Mock<HttpContextBase>();
+                httpContextMock.Setup(x => x.Request.Url)
+                    .Returns(new Uri("http://example.com/femah.axd/api/unknownservicebla"));
+                httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("GET");
+
+                var response = new Mock<HttpResponseBase>();
+                response.SetupProperty(x => x.StatusCode);
+                httpContextMock.Setup(x => x.Response).Returns(response.Object);
+
+                //Act
+                testable.ProcessRequest(httpContextMock.Object);
+
+                response.Object.StatusCode.ShouldBe(405);
+            }
+
             [Test]
             public void ReturnsCorrectEncodingAndContentType()
             {
@@ -29,6 +51,39 @@ namespace Femah.Core.Tests
 
                 response.Object.ContentType.ShouldBe("application/json");
                 response.Object.ContentEncoding.ShouldBe(Encoding.UTF8);
+            }
+
+            [Test]
+            public void Returns405AndAccurateErrorMessage_IfServiceDoesntSupportParameterQuerying()
+            {
+                var testable = new TestableFemahApiHttpHandler();
+                var httpContextMock = new Mock<HttpContextBase>();
+                httpContextMock.Setup(x => x.Request.Url)
+                    .Returns(new Uri("http://example.com/femah.axd/api/featureswitchtypes/simplefeatureswitch"));
+                httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("GET");
+
+                var response = new Mock<HttpResponseBase>();
+                response.SetupProperty(x => x.StatusCode);
+                httpContextMock.Setup(x => x.Response).Returns(response.Object);
+
+                var featureSwitchTypes = new[] { typeof(SimpleFeatureSwitch), typeof(SimpleFeatureSwitch) };
+
+                const string expectedJsonResponse = "\"Error: Service 'featureswitchtypes' does not support parameter querying.\"";
+
+                Femah.Configure()
+                    .WithSelectedFeatureSwitchTypes(featureSwitchTypes)
+                    .Initialise();
+
+                //Get the JSON response by intercepting the call to context.Response.Write
+                var responseContent = string.Empty;
+                response.Setup(x => x.Write(It.IsAny<string>())).Callback((string r) => { responseContent = r; });
+
+                //Act
+                testable.ProcessRequest(httpContextMock.Object);
+
+                //Asert
+                response.Object.StatusCode.ShouldBe(405);
+                responseContent.ShouldBe(expectedJsonResponse);
             }
         }
     }
--- Femah.Core.Tests/WhenAccessingTheService.cs
+++ Femah.Core.Tests/WhenAccessingTheService.cs
@@ -4,66 +4,12 @@ using NUnit.Framework;
 using Shouldly;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Web;
 
 namespace Femah.Core.Tests
 {
     public class WhenAccessingTheService
     {
-        [Test]
-        public void AndServiceDoesNotSupportParameterQuerying_ThenGetReturns405AndAccurateErrorMessage()
-        {
-            var testable = new TestableFemahApiHttpHandler();
-            var httpContextMock = new Mock<HttpContextBase>();
-            httpContextMock.Setup(x => x.Request.Url)
-                .Returns(new Uri("http://example.com/femah.axd/api/featureswitchtypes/simplefeatureswitch"));
-            httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("GET");
-
-            var response = new Mock<HttpResponseBase>();
-            response.SetupProperty(x => x.StatusCode);
-            httpContextMock.Setup(x => x.Response).Returns(response.Object);
-
-            var featureSwitchTypes = new[] {typeof (SimpleFeatureSwitch), typeof (SimpleFeatureSwitch)};
-
-            const string expectedJsonResponse = "\"Error: Service 'featureswitchtypes' does not support parameter querying.\"";
-
-            Femah.Configure()
-                .WithSelectedFeatureSwitchTypes(featureSwitchTypes)
-                .Initialise();
-
-            //Get the JSON response by intercepting the call to context.Response.Write
-            var responseContent = string.Empty;
-            response.Setup(x => x.Write(It.IsAny<string>())).Callback((string r) => { responseContent = r; });
-
-            //Act
-            testable.ProcessRequest(httpContextMock.Object);
-
-            //Asert
-            response.Object.StatusCode.ShouldBe(405);
-            responseContent.ShouldBe(expectedJsonResponse);
-        }
-
-        [Test]
-        public void AndTheServiceIsNotFound_ThenGetReturns405()
-        {
-            //Arrange
-            var testable = new TestableFemahApiHttpHandler();
-            var httpContextMock = new Mock<HttpContextBase>();
-            httpContextMock.Setup(x => x.Request.Url)
-                .Returns(new Uri("http://example.com/femah.axd/api/unknownservicebla"));
-            httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("GET");
-
-            var response = new Mock<HttpResponseBase>();
-            response.SetupProperty(x => x.StatusCode);
-            httpContextMock.Setup(x => x.Response).Returns(response.Object);
-
-            //Act
-            testable.ProcessRequest(httpContextMock.Object);
-
-            response.Object.StatusCode.ShouldBe(405);
-        }
-
         [Test]
         public void AndTheServiceExists_ThenGetReturns200()
         {
