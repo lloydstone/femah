29c39da3f63043a858426576614a428b417c8617
AwaNoodle <mark.nick.turner@gmail.com>
2014-04-22 08:20:58 +0100

    Factored out some of the common ApiRequestBuilder test code

--- Femah.Core.Tests/ApiRequestBuilderTests.cs
+++ Femah.Core.Tests/ApiRequestBuilderTests.cs
@@ -13,22 +13,29 @@ namespace Femah.Core.Tests
     {
         public class TheBuildMethod
         {
+            private Mock<HttpContextBase> _httpContextMock;
+            private ApiRequestBuilder _sut;
+
+            [SetUp]
+            public void Init()
+            {
+                _httpContextMock = new Mock<HttpContextBase>();
+                _sut = new ApiRequestBuilder();
+            }
+
             [Test]
             public void SetsHttpStatusCodeTo415AndProvidesAccurateErrorMessage_IfContentTypeIsNotSetToApplicationJsonAndRequestIsAPut()
             {
                 //Arrange
-                var testable = new ApiRequestBuilder();
-
-                var httpContextMock = new Mock<HttpContextBase>();
-                httpContextMock.Setup(x => x.Request.Url)
+                _httpContextMock.Setup(x => x.Request.Url)
                     .Returns(new Uri("http://example.com/femah.axd/api/featureswitches/TestFeatureSwitch1"));
-                httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("PUT");
-                httpContextMock.SetupGet(x => x.Request.ContentType).Returns("incorrect/contenttype");
+                _httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("PUT");
+                _httpContextMock.SetupGet(x => x.Request.ContentType).Returns("incorrect/contenttype");
 
                 const string expectedJsonBody = "Error: Content-Type 'incorrect/contenttype' of request is not supported, expecting 'application/json'.";
 
                 //Act
-                ApiRequest apiRequest = testable.Build(httpContextMock.Object.Request);
+                ApiRequest apiRequest = _sut.Build(_httpContextMock.Object.Request);
 
                 //Asert
                 Assert.AreEqual(HttpStatusCode.UnsupportedMediaType, apiRequest.ErrorMessageHttpStatusCode);
@@ -39,29 +46,32 @@ namespace Femah.Core.Tests
             public void SetsApiRequestBody_IfHttpContextInputStreamIsNotNull()
             {
                 //Arrange
-                var testable = new ApiRequestBuilder();
-
-                var httpContextMock = new Mock<HttpContextBase>();
-                httpContextMock.Setup(x => x.Request.Url)
+                _httpContextMock.Setup(x => x.Request.Url)
                     .Returns(new Uri("http://example.com/femah.axd/api/featureswitches"));
-                httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("GET");
+                _httpContextMock.SetupGet(x => x.Request.HttpMethod).Returns("GET");
 
                 //Build the request Body in JSON
                 const string expectedJsonBody =
                     "{\"IsEnabled\":true,\"Name\":\"TestFeatureSwitch1\",\"FeatureType\":\"SimpleFeatureSwitch\",\"Description\":\"Define a short description of the feature switch type here.\",\"ConfigurationInstructions\":\"Add configuration context and instructions to be displayed in the admin UI\"}";
                 
+                var inputStream = CreateInputStream(expectedJsonBody);
+
+                _httpContextMock.SetupGet(x => x.Request.InputStream).Returns(inputStream);
+
+                //Act
+                ApiRequest apiRequest = _sut.Build(_httpContextMock.Object.Request);
+
+                //Assert
+                Assert.AreEqual(expectedJsonBody, apiRequest.Body);
+            }
+
+            private static MemoryStream CreateInputStream(string expectedJsonBody)
+            {
                 var inputStream = new MemoryStream();
                 var streamWriter = new StreamWriter(inputStream);
                 streamWriter.Write(expectedJsonBody);
                 streamWriter.Flush();
-
-                httpContextMock.SetupGet(x => x.Request.InputStream).Returns(inputStream);
-
-                //Act
-                ApiRequest apiRequest = testable.Build(httpContextMock.Object.Request);
-
-                //Assert
-                Assert.AreEqual(expectedJsonBody, apiRequest.Body);
+                return inputStream;
             }
         }
     }
